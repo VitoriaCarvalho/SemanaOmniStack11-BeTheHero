307e03ffc543d4283100610150975a74bd3279b6
VitoriaCarvalho <vitoriiacb@gmail.com>
2020-03-27 01:52:22 -0300

    Front-end concluído.

--- be-the-hero/backend/src/index.js
+++ be-the-hero/backend/src/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const routes = require('./routes')
 const cors = require('cors');
+const routes = require('./routes')
 const app = express();
 
+app.use(cors());
 app.use(express.json()); //pra que o formato json possa ser entendido
 app.use(routes);
-app.use(cors());
 
 app.listen(3333); //Quando eu acessar localhost:3333, estarei acessando minha aplicação. Poderia usar a porta 80 (pra usar somente localhost), mas pode ser problemática em alguns SOs.
